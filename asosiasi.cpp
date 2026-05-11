51d510a22d8038b5fa5ea7a68cb2eb404c67cd51
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-05-11 10:55:05 +0700

    mengisi class dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -17,5 +17,17 @@ class pasien { // asosiasi
     void cetakDokter();
 };
 class dokter {
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
 
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
 };
