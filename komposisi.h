cda3e24fd6dfec234ffd46f4aa2795449a54afbc
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-05-11 11:08:48 +0700

    membuat file komposisi.h

---
+++ komposisi.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "jantung.h"
+#include "manusia.h"
+
+int main()
+{
+    manusia *varManusia = new manusia("Markus");
+    delete varManusia;
+
+    return 0;
+}
